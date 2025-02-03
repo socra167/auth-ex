72019f4d0fe236d1cf92886ce8626901b51e69c7
socra167 <socra167@gmail.com>
2025-02-03 10:46:05 +0900

    feat: 글을 등록한 사람만 자신의 글을 삭제할 수 있다

--- src/main/java/com/auth/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/auth/domain/post/post/controller/ApiV1PostController.java
@@ -17,109 +17,119 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class ApiV1PostController {
 
-    private final PostService postService;
-    private final MemberService memberService;
+	private final PostService postService;
+	private final MemberService memberService;
 
-    @GetMapping
-    public RsData<List<PostDto>> getItems() {
+	@GetMapping
+	public RsData<List<PostDto>> getItems() {
 
-        List<Post> posts = postService.getItems();
-        List<PostDto> postDtos = posts.stream()
-                .map(PostDto::new)
-                .toList();
+		List<Post> posts = postService.getItems();
+		List<PostDto> postDtos = posts.stream()
+			.map(PostDto::new)
+			.toList();
 
-        return new RsData<>(
-                "200-1",
-                "글 목록 조회가 완료되었습니다.",
-                postDtos
-        );
-    }
+		return new RsData<>(
+			"200-1",
+			"글 목록 조회가 완료되었습니다.",
+			postDtos
+		);
+	}
 
+	@GetMapping("{id}")
+	public RsData<PostDto> getItem(@PathVariable long id) {
 
-    @GetMapping("{id}")
-    public RsData<PostDto> getItem(@PathVariable long id) {
+		Post post = postService.getItem(id).get();
 
-        Post post = postService.getItem(id).get();
+		return new RsData<>(
+			"200-1",
+			"글 조회가 완료되었습니다.",
+			new PostDto(post)
+		);
+	}
 
-        return new RsData<>(
-                "200-1",
-                "글 조회가 완료되었습니다.",
-                new PostDto(post)
-        );
-    }
+	record DeleteReqBody(@NotNull Long authorId, @NotBlank @Length(min = 3) String password) {
+	}
 
-    @DeleteMapping("/{id}")
-    public RsData<Void> delete(@PathVariable long id) {
-        Post post = postService.getItem(id).get();
-        postService.delete(post);
+	@DeleteMapping("/{id}")
+	public RsData<Void> delete(@PathVariable long id, @RequestBody @Valid DeleteReqBody body) {
+		// 인증
+		Member actor = memberService.findById(body.authorId()).get();
+		if (!actor.getPassword().equals(body.password)) { // 비밀번호 검사
+			throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
+		}
 
-        return new RsData<>(
-                "204-1",
-                "%d번 글 삭제가 완료되었습니다.".formatted(id)
-        );
-    }
+		Post post = postService.getItem(id).get();
 
+		// 자신이 등록한 글만 삭제할 수 있다 : 인가
+		if (post.getAuthor().getId() != body.authorId()) {
+			throw new ServiceException("403-1", "자신이 작성한 글만 삭제 가능합니다.");
+		}
 
-    record ModifyReqBody(
-        @NotBlank @Length(min = 3) String title,
-        @NotBlank @Length(min = 3) String content,
-        @NotNull Long authorId,
-        @NotBlank @Length(min = 3) String password) {
-    }
+		postService.delete(post);
 
-    @PutMapping("/{id}")
-    public RsData<Void> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body) {
+		return new RsData<>(
+			"204-1",
+			"%d번 글 삭제가 완료되었습니다.".formatted(id)
+		);
+	}
 
-        // 인증
-        Member actor = memberService.findById(body.authorId()).get();
-        if (!actor.getPassword().equals(body.password)) { // 비밀번호 검사
-            throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
-        }
+	record ModifyReqBody(
+		@NotBlank @Length(min = 3) String title,
+		@NotBlank @Length(min = 3) String content,
+		@NotNull Long authorId,
+		@NotBlank @Length(min = 3) String password) {
+	}
 
-        Post post = postService.getItem(id).get();
+	@PutMapping("/{id}")
+	public RsData<Void> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body) {
+		// 인증
+		Member actor = memberService.findById(body.authorId()).get();
+		if (!actor.getPassword().equals(body.password)) { // 비밀번호 검사
+			throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
+		}
 
-        // 자신이 등록한 글만 수정할 수 있다 : 인가
-        if (post.getAuthor().getId() != body.authorId()) {
-            throw new ServiceException("403-1", "자신이 작성한 글만 수정 가능합니다.");
-        }
+		Post post = postService.getItem(id).get();
 
-        postService.modify(post, body.title(), body.content());
-        return new RsData<>(
-                "200-1",
-                "%d번 글 수정이 완료되었습니다.".formatted(id),
-                null
-        );
-    }
+		// 자신이 등록한 글만 수정할 수 있다 : 인가
+		if (post.getAuthor().getId() != body.authorId()) {
+			throw new ServiceException("403-1", "자신이 작성한 글만 수정 가능합니다.");
+		}
 
+		postService.modify(post, body.title(), body.content());
+		return new RsData<>(
+			"200-1",
+			"%d번 글 수정이 완료되었습니다.".formatted(id),
+			null
+		);
+	}
 
-    record WriteReqBody(
-        @NotBlank @Length(min = 3) String title,
-        @NotBlank @Length(min = 3) String content,
-        @NotNull Long authorId, // validation 체크를 하려면 원시타입이 아닌 객체 타입이어야 한다
-        @NotBlank @Length(min = 3) String password) { // 글 작성을 위해 요청을 보낸 사람이 본인임을 인증하기 위해 비밀번호를 받도록 한다
-    }
+	record WriteReqBody(
+		@NotBlank @Length(min = 3) String title,
+		@NotBlank @Length(min = 3) String content,
+		@NotNull Long authorId, // validation 체크를 하려면 원시타입이 아닌 객체 타입이어야 한다
+		@NotBlank @Length(min = 3) String password) { // 글 작성을 위해 요청을 보낸 사람이 본인임을 인증하기 위해 비밀번호를 받도록 한다
+	}
 
-    @PostMapping
-    public RsData<PostDto> write(@RequestBody @Valid WriteReqBody body) {
+	@PostMapping
+	public RsData<PostDto> write(@RequestBody @Valid WriteReqBody body) {
 
-        Member actor = memberService.findById(body.authorId()).get();
+		Member actor = memberService.findById(body.authorId()).get();
 
-        if (!actor.getPassword().equals(body.password)) { // 비밀번호 검사
-            throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
-        }
+		if (!actor.getPassword().equals(body.password)) { // 비밀번호 검사
+			throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
+		}
 
-        Post post = postService.write(actor, body.title(), body.content());
+		Post post = postService.write(actor, body.title(), body.content());
 
-        return new RsData<>(
-                        "200-1",
-                        "글 작성이 완료되었습니다.",
-                        new PostDto(post)
-                );
-    }
+		return new RsData<>(
+			"200-1",
+			"글 작성이 완료되었습니다.",
+			new PostDto(post)
+		);
+	}
 }
