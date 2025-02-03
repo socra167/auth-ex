1cd10c2fd8b811073723a4edae68279614ff31f6
socra167 <socra167@gmail.com>
2025-02-03 15:16:48 +0900

    feat: 로그인을 성공하면 `apiKey`를 응답하도록 로그인 구현

--- src/main/java/com/auth/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/auth/domain/member/member/controller/ApiV1MemberController.java
@@ -21,30 +21,47 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApiV1MemberController {
 
-    private final MemberService memberService;
+	private final MemberService memberService;
 
-    record JoinReqBody(@NotBlank @Length(min = 3) String username,
-                       @NotBlank @Length(min = 3) String password,
-                       @NotBlank @Length(min = 3) String nickname) {
-    }
+	record JoinReqBody(@NotBlank @Length(min = 3) String username,
+					   @NotBlank @Length(min = 3) String password,
+					   @NotBlank @Length(min = 3) String nickname) {
+	}
 
-    @PostMapping("/join")
-    public RsData<MemberDto> join(@RequestBody @Valid JoinReqBody body) {
+	@PostMapping("/join")
+	public RsData<MemberDto> join(@RequestBody @Valid JoinReqBody body) {
 
+		memberService.findByUsername(body.username())
+			.ifPresent(member -> {
+				throw new ServiceException("400-1", "중복된 아이디입니다.");
+			});
 
-        memberService.findByUsername(body.username())
-                .ifPresent(member -> {
-                    throw new ServiceException("400-1", "중복된 아이디입니다.");
-                });
+		Member member = memberService.join(body.username(), body.password(), body.nickname());
 
+		return new RsData<>(
+			"201-1",
+			"회원 가입이 완료되었습니다.",
+			new MemberDto(member)
+		);
+	}
 
-        Member member = memberService.join(body.username(), body.password(), body.nickname());
+	record LoginReqBody(@NotBlank @Length(min = 3) String username,
+						@NotBlank @Length(min = 3) String password) {
+	}
 
-        return new RsData<>(
-                "201-1",
-                "회원 가입이 완료되었습니다.",
-                new MemberDto(member)
-        );
-    }
+	@PostMapping("/login")
+	public RsData<String> login(@RequestBody @Valid LoginReqBody body) {
+		Member actor = memberService.findByUsername(body.username())
+			.orElseThrow(() -> new ServiceException("401-2", "아이디 또는 비밀번호가 일치하지 않습니다."));
 
+		if (!actor.getPassword().equals(body.password())) {
+			throw new ServiceException("401-2", "아이디 또는 비밀번호가 일치하지 않습니다.");
+		}
+
+		return new RsData<>(
+			"200-1",
+			"%s님 환영합니다.".formatted(actor.getNickname()),
+			actor.getApiKey()
+		);
+	}
 }
