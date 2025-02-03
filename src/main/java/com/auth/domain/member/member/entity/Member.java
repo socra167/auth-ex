41280c4db439f39e49170a6294fbc360b658789f
socra167 <socra167@gmail.com>
2025-02-03 14:41:25 +0900

    fix: `password2` `unique` 적용

--- src/main/java/com/auth/domain/member/member/entity/Member.java
+++ src/main/java/com/auth/domain/member/member/entity/Member.java
@@ -20,7 +20,7 @@ public class Member extends BaseTime {
     private String username;
     @Column(length = 100)
     private String password;
-    @Column(length = 100)
+    @Column(length = 100, unique = true)
     private String password2;
     @Column(length = 100)
     private String nickname;
