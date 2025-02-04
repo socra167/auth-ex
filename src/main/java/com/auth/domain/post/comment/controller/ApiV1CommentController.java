42b0700e01efbce3a1c7a38077a4f3c9f2889898
socra167 <socra167@gmail.com>
2025-02-04 09:13:56 +0900

    feat: `ApiV1CommentController`

---
+++ src/main/java/com/auth/domain/post/comment/controller/ApiV1CommentController.java
@@ -0,0 +1,14 @@
+package com.auth.domain.post.comment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/api/v1/comments")
+public class ApiV1CommentController {
+
+
+}
